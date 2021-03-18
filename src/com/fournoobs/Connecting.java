6a8c03ef51fc1bf0af5cf2e5ff7b1ec933d3e2b2
Jonathan Rotter <hydrogen31415@gmail.com>
2021-03-19 00:05:36 +0100

    translated python into java

--- src/com/fournoobs/Connecting.java
+++ src/com/fournoobs/Connecting.java
@@ -2,13 +2,45 @@ package com.fournoobs;
 
 import krpc.client.Connection;
 import krpc.client.RPCException;
-import krpc.client.services.KRPC;
+// import krpc.client.services.KRPC;
+import krpc.client.services.SpaceCenter;
+
 import java.io.IOException;
 
 public class Connecting {
-    public static void main() throws IOException, RPCException {
-        try (Connection connection = Connection.newInstance("Remote example", "my.domain.name", 1000, 1001)) {
-            System.out.println(KRPC.newInstance(connection).getStatus().getVersion());
-        }
+
+    private final Connection conn;
+    //private final KRPC krpc;
+    private final SpaceCenter spaceCenter;
+    private final SpaceCenter.Vessel vessel;
+
+    public Connecting(String name, String host, int rpcPort, int streamPort) throws IOException, RPCException {
+        conn = Connection.newInstance(name, host, rpcPort, streamPort);
+        //krpc = KRPC.newInstance(conn);
+        spaceCenter = SpaceCenter.newInstance(conn);
+        vessel = spaceCenter.getActiveVessel();
+        System.out.println(vessel.getName());
     }
+
+    public SpaceCenter.Vessel getVessel() {
+        return vessel;
+    }
+
+    public SpaceCenter getSpaceCenter() {
+        return spaceCenter;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    //    public static void main() throws IOException, RPCException {
+//        try (Connection connection = Connection.newInstance("Suborbital Test", "localhost", 50000, 50001)) {
+//            KRPC k = KRPC.newInstance(connection);
+//            SpaceCenter spaceCenter = SpaceCenter.newInstance(connection);
+//            SpaceCenter.Vessel vessel = spaceCenter.getActiveVessel();
+//            System.out.println(vessel.getName());
+//            System.out.println(k.getStatus().getVersion());
+//        }
+//    }
 }
--- src/com/fournoobs/Main.java
+++ src/com/fournoobs/Main.java
@@ -2,11 +2,28 @@ package com.fournoobs;
 
 public class Main {
 
+    private static final int[] heights = {50, 100, 200, 400, 800};
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("test");
         try {
-            Connecting.main();
+            Connecting c = new Connecting("Suborbital Hop", "localhost", 50000, 50001);
+            c.getVessel().getControl().activateNextStage();
+            c.getSpaceCenter().quicksave();
+
+            for (int h: heights) {
+                c.getSpaceCenter().quickload();
+                HopperMK1 mk1 = new HopperMK1(c.getVessel(), c.getConn());
+                mk1.shutoffHeight = h;
+                System.out.println("Aiming for " + h);
+
+                while (mk1.update()) {}
+
+                Thread.sleep(5000);
+            }
+
+
         }
         catch (Exception e) {
             e.printStackTrace();
