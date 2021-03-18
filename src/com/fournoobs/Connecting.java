45a852ca916706c12deac08fdf4e28e1761fad40
Jonathan Rotter <hydrogen31415@gmail.com>
2021-03-18 16:29:00 +0100

    java setup in intellij

---
+++ src/com/fournoobs/Connecting.java
@@ -0,0 +1,14 @@
+package com.fournoobs;
+
+import krpc.client.Connection;
+import krpc.client.RPCException;
+import krpc.client.services.KRPC;
+import java.io.IOException;
+
+public class Connecting {
+    public static void main() throws IOException, RPCException {
+        try (Connection connection = Connection.newInstance("Remote example", "my.domain.name", 1000, 1001)) {
+            System.out.println(KRPC.newInstance(connection).getStatus().getVersion());
+        }
+    }
+}
---
+++ src/com/fournoobs/Main.java
@@ -0,0 +1,16 @@
+package com.fournoobs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("test");
+        try {
+            Connecting.main();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
